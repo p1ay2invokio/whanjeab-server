399e804e6844c85bb4e97d16b25ed5cf8750342f
q1ay2.xyz <66400989+p1ay2invokio@users.noreply.github.com>
2025-12-05 14:10:31 +0700

    import line index

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import keyRoute from './routes/key.route'
 import userRoute from './routes/user.route'
+import lineRoute from './routes/line.route'
 
 let env = dotenv.config().parsed
 
@@ -11,7 +12,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/api', keyRoute, userRoute)
+app.use('/api', keyRoute, userRoute, lineRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
