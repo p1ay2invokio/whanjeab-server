ad497727f51ae80ed225545442d65de716fb2d47
q1ay2.xyz <66400989+p1ay2invokio@users.noreply.github.com>
2025-12-19 19:04:17 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 export let is_dev = false
 export let client_url = is_dev ? 'http://localhost:3000' : 'https://whanjeabs.online'
-export let server_url = is_dev ? 'https://localhost:3001' : 'http://api.whanjeabs.online'
+export let server_url = is_dev ? 'https://localhost:3001' : 'https://api.whanjeabs.online'
